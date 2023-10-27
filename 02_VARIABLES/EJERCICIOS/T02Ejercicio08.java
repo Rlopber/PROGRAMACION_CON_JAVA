4dec54d9119316afecd18d2f416ee973a2c1dc08
Rlopber <rlopber070@g.educaand.es>
2023-10-27 15:38:25 +0200

    Terminado el T02Ejercicio08

--- 02_VARIABLES/EJERCICIOS/T02Ejercicio08.java
+++ 02_VARIABLES/EJERCICIOS/T02Ejercicio08.java
@@ -7,5 +7,15 @@
 public class T02Ejercicio08 {
     public static void main(String[] args) {
         
+    char letra1='p';
+    char letra2='e';
+    char letra3='r';
+    char letra4='r';
+    char letra5='o';
+
+    System.out.println(""+ letra1 + letra2 + letra3 + letra4 + letra5);
+
+    // Para que concatene las letras hay que añadir al principio de la frase dobles comillas "".
+    // Si no, lo que pasaría es que suma el codigo ascii de esas letras.
     }
 }
