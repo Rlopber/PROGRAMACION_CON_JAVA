76e2e74fbb7722db321db5e3250134761842f853
Rlopber <rlopber070@g.educaand.es>
2023-10-27 16:31:18 +0200

    Añadido ejemplo double y float

--- 03_LECTURA_DE_DATOS_DESDE_TECLADO/EJEMPLOS/T03Ejemplo02.java
+++ 03_LECTURA_DE_DATOS_DESDE_TECLADO/EJEMPLOS/T03Ejemplo02.java
@@ -21,10 +21,29 @@ public class T03Ejemplo02 {
     int num2;
     num2 = Integer.parseInt(linea);
 
-    System.out.printf("El primer número es: %d.5 \n", num1);
+    System.out.printf("El primer número es: %d. \n", num1);
     System.out.printf("El segundo número es: %d. \n", num2);
 
-    System.out.println("EL doble del número " + num1 + " es " + (num1*2) + ".");
+    System.out.println("El doble del número " + num1 + " es " + (num1*2) + ".");
+
+    System.out.println("// // // // // //");
+
+    System.out.println("Introduce un número real.");
+    linea = System.console().readLine();
+
+    double numReal1;
+    numReal1 = Double.parseDouble(linea);
+
+    System.out.println("Introduce un número real.");
+    linea = System.console().readLine();
+
+    float numReal2;
+    numReal2 = Float.parseFloat(linea);
+
+    System.out.printf("El primer número es: %.2f \n", numReal1);
+    System.out.printf("El segundo número es: %.2f \n", numReal2);
+
+    System.out.println("El doble del número " + numReal1 + " es " + (numReal1*2) + ".");
 
     } 
 }
