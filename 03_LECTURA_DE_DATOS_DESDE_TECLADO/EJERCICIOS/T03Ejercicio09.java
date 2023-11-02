f173b00eb9440082ff8a21f3139fc7138805957b
Raquel <rlopber070@g.educaand.es>
2023-11-02 11:33:24 +0100

    Añadido T03Ejercicio09

---
+++ 03_LECTURA_DE_DATOS_DESDE_TECLADO/EJERCICIOS/T03Ejercicio09.java
@@ -0,0 +1,21 @@
+/**
+*
+* Programa que calcule el volumen de un cono.
+*
+* @author: Raquel Lopez
+*/
+
+public class T03Ejercicio09 {
+    public static void main(String[] args) {
+        
+        System.out.println("Vamos a calccular el volumen de un cono.");
+
+        System.out.print("¿Cuál es el radio del cono (cm)?: ");
+        float radio = Float.parseFloat(System.console().readLine());
+
+        System.out.print("¿Cuál es la altura del cono (cm)?: ");
+        float altura = Float.parseFloat(System.console().readLine());
+
+        System.out.printf("EL volumen del cono es %.2f cm³.\n", ((altura*radio*radio*Math.PI)/3));
+    }
+}
