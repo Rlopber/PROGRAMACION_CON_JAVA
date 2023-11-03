30a1d8819c0b747b19ede89cb21c1febe4b2b027
Rlopber <rlopber070@g.educaand.es>
2023-11-03 15:45:52 +0100

    Añadido T04Ejemplo01

---
+++ 04_SENTENCIAS_CONDICIONAL/EJEMPLOS/T04Ejemplo01.java
@@ -0,0 +1,17 @@
+/**
+*
+* Uso del programa.
+*
+* @author Raquel Lopez
+*/
+
+public class T04Ejemplo01 {
+    public static void main(String[] args) {
+        int time = 5;
+        if (time<10) {
+            System.out.println("Good morning.");
+        } else if (time<18) {
+            System.out.println("Good day.");
+        }
+    }
+}
