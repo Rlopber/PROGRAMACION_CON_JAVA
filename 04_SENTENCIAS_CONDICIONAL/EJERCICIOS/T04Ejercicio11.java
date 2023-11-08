8dcd69c673fc70d82608905df0454074a0191b5b
Rlopber <rlopber070@g.educaand.es>
2023-11-08 17:06:26 +0100

    Añadido T04Ejercicio11

---
+++ 04_SENTENCIAS_CONDICIONAL/EJERCICIOS/T04Ejercicio11.java
@@ -0,0 +1,29 @@
+/**
+*
+* Escribe un programa que dada una hora determinada (horas y minutos), calcule los segundos que faltan para llegar a la medianoche.
+*
+* @author Raquel Lopez
+*/
+import java.util.Scanner;
+
+
+public class T04Ejercicio11 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Este programa te dice los segundos que quedan para la media noche a partir de la hora que quieras.");
+        System.out.print("Dime la hora (0 a 23): ");
+        int hora = s.nextInt();
+        System.out.print("Dime los minutos (0 a 59): ");
+        int minuto = s.nextInt();
+
+        if ((hora>24 || hora<0) || (minuto>59 || minuto<0)) {
+            System.out.println("Alguno de los los valores introducidos no es válido.");
+        } else {
+            int segundosMedianoche = (23 - hora) * 3600 + (59 - minuto) * 60;
+            System.out.printf("Faltan %d segundos para la medianoche.\n", segundosMedianoche);
+        }
+
+        s.close();
+    }
+}
