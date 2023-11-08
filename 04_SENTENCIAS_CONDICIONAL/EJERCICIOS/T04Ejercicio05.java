4c00dc5fcec61331224dd58c8e6723cba4046afd
Rlopber <rlopber070@g.educaand.es>
2023-11-08 15:26:44 +0100

    T0405

---
+++ 04_SENTENCIAS_CONDICIONAL/EJERCICIOS/T04Ejercicio05.java
@@ -0,0 +1,13 @@
+/**
+*
+* Programa que resuelva una ecuación de primer grado (ax+b=0).
+*
+* @author Raquel Lopez
+*/
+
+public class T04Ejercicio05 {
+    public static void main(String[] args) {
+        System.out.println("Este programa resuelve ecuaciones de primer grado del tipo ax+b = 0");
+        System.out.println();
+    }
+}
