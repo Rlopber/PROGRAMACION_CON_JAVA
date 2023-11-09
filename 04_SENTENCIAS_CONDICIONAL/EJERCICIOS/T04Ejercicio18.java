0c40d577f4925fa3839e9517cef9b394e24f2605
Rlopber <rlopber070@g.educaand.es>
2023-11-09 20:25:08 +0100

    Creada variable auxiliar T04Ejercicio18

--- 04_SENTENCIAS_CONDICIONAL/EJERCICIOS/T04Ejercicio18.java
+++ 04_SENTENCIAS_CONDICIONAL/EJERCICIOS/T04Ejercicio18.java
@@ -13,20 +13,21 @@ public class T04Ejercicio18 {
 
         System.out.print("Por favor, introduzca un número entero (5 cifras como máximo): ");
         int numero = s.nextInt();
-        int primeraCifra = Math.abs(numero); //Almacena la primera cifra del numero.
+        int numeroAbs = Math.abs(numero);
+        int primeraCifra = 0; //Almacena la primera cifra del numero.
         s.close();
         
         // Comprobación del número de cifras y de la primera cifra.
         if (primeraCifra < 10) {
-            System.out.println(""); // La primera cifra es ya es el número.
+            primeraCifra = numeroAbs;
         } else if (primeraCifra>=10 && primeraCifra<100) {
-            primeraCifra /= 10; // (primeraCifra /= 100) es lo mismo que (primeraCifra = primeraCifra/100)
+            primeraCifra = numeroAbs/10; 
         } else if (primeraCifra>=100 && primeraCifra<1000) {
-            primeraCifra /= 100; 
+            primeraCifra = numeroAbs/100; 
         } else if (primeraCifra>=1000 && primeraCifra<10000) {
-            primeraCifra /= 1000;
+            primeraCifra = numeroAbs/1000;
         } else if (primeraCifra>=10000 && primeraCifra<100000) {
-            primeraCifra /= 10000;
+            primeraCifra = numeroAbs/10000;
         } else {
             System.out.println("El número introducido tiene más de 5 cifras.");
             return; //Sale del programa si tiene más de 5 cifras.
