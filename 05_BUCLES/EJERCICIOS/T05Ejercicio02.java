08a339cf66cafafabfaa456997d2bcbaf6afcb0f
Rlopber <rlopber070@g.educaand.es>
2023-11-14 18:28:41 +0100

    Añadido T05Ejercicio01 y T05Ejercicio02

--- 05_BUCLES/EJERCICIOS/T05Ejercicio02.java
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio02.java
@@ -1,15 +1,19 @@
 /**
 *
-* Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+* Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
 *
 * @author Raquel Lopez
 */
 
-public class T05Ejercicio01 {
+public class T05Ejercicio02 {
     public static void main(String[] args) {
         System.out.println("Este programa muestra los números múltiplos de 5 del 0 al 100.");
 
-        for (int i = 0; i < 101; i += 5)
+        int i = 0;
+
+        while (i < 101) {
         System.out.print(i + " ");
+        i +=5 ;
+        }
     }
 }
