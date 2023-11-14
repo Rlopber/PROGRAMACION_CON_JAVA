c0395d3ad5c63adf4671024476d1569d932fa250
Rlopber <rlopber070@g.educaand.es>
2023-11-14 16:51:06 +0100

    Añadida suma de positivos en T05EJ04

--- 05_BUCLES/EJEMPLOS/T05EJ04.java
+++ 05_BUCLES/EJEMPLOS/T05EJ04.java
@@ -15,18 +15,21 @@ public class T05EJ04 {
         System.out.println("Contador de números positivos.");
 
         int numIntroducido = 0; // Variable contador para la salida del bucle
-        int contador = 0; // Variable acumuladora de números enteros positivos.
+        int contador = 0;       // Variable contadora de números enteros positivos.
+        int suma = 0;            // Variable acumuladora de números positivos.
 
         System.out.println("Escribe números enteros positivos. El contador parará cuando introduzcas un número negativo.");
         while (numIntroducido >= 0) {
             System.out.print("Número: ");
             numIntroducido = s.nextInt();
-            if (numIntroducido > 0) { // Si el número es mayor a 0 suma 1 al contador numPositivos.
+            if (numIntroducido > 0) {       // Si el número es mayor a 0 suma 1 al contador numPositivos. 
                 contador++;
+                suma += numIntroducido;     // suma = suma + numeroIntroducido    
             }
         }
 
         s.close();
         System.out.println("Has introducido " + contador + " números positivos.");
+        System.out.println("La suma de los números positivos introducidos es " + suma + ".");
     }
 }
