8e2044f32ff0443f9ef449ba4ad26b30438e5a29
Raquel <rlopber070@g.educaand.es>
2023-11-16 15:09:57 +0100

    Añadido T05Ejercicio11

---
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio11.java
@@ -0,0 +1,30 @@
+/**
+*
+* Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de los 5 primeros números enteros a partir de uno que se introduce por teclado.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T05Ejercicio11 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        
+        int numero;
+
+        System.out.println("Con este programa podrás resolver potencias de 2 y 3 para los 5 primeros números enteros a partir del que va a introducir.");
+        System.out.print("Introduzca el números entero que quiera: ");
+        numero = s.nextInt();
+        s.close();
+
+        System.out.println("    n    |    n\u00B2    |    n\u00B3");
+        System.out.println("-------------------------------");
+
+        for (int i = numero; i < numero+5; i++)
+        
+        System.out.printf("%8d |%8.0f  |%9.0f\n", i, Math.pow(i,2), Math.pow(i,3));
+        
+    }
+}
