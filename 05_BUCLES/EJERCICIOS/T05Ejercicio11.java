e9216e50a9427f380baa6dccec9a4163ca0492b2
Raquel <rlopber070@g.educaand.es>
2023-11-16 15:11:52 +0100

    Añadido comentarios T05Ejercicio11

--- 05_BUCLES/EJERCICIOS/T05Ejercicio11.java
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio11.java
@@ -12,8 +12,10 @@ public class T05Ejercicio11 {
 
         Scanner s = new Scanner(System.in);
         
+        // Declarar variables
         int numero;
 
+        // Pedir datos
         System.out.println("Con este programa podrás resolver potencias de 2 y 3 para los 5 primeros números enteros a partir del que va a introducir.");
         System.out.print("Introduzca el números entero que quiera: ");
         numero = s.nextInt();
@@ -22,6 +24,7 @@ public class T05Ejercicio11 {
         System.out.println("    n    |    n\u00B2    |    n\u00B3");
         System.out.println("-------------------------------");
 
+        // Generar la tabla
         for (int i = numero; i < numero+5; i++)
         
         System.out.printf("%8d |%8.0f  |%9.0f\n", i, Math.pow(i,2), Math.pow(i,3));
