c0dea4956795aa27326010b0020f363d6a2bdb43
Raquel <rlopber070@g.educaand.es>
2023-11-20 12:20:02 +0100

    Añadido T05Ejercicio26

---
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio26.java
@@ -0,0 +1,54 @@
+/**
+*
+* Realiza un programa que pida primero un número y a continuación un dígito. 
+* El programa nos debe dar la posición (o posiciones) contando de izquierda a derecha que ocupa ese dígito en el número introducido.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T05Ejercicio26 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        //Desclarar variables
+        int posicion = 1;
+        boolean encontrado = false;
+
+        //Descripción del programa y entrada de datos
+        System.out.println("Este programa te dirá la posición de un dígito que quieras dentro de un número que elijas.");
+        System.out.print("¿En qué número vamos a buscar?: ");
+        int numeroIntroducido = s.nextInt();
+        System.out.print("¿De qué dígito quieres saber la posición?: ");
+        int digito = s.nextInt();
+        s.close();
+
+        //Averiguar la posición
+        int numero = numeroIntroducido; //Para no modificar el valor de numeroIntroducido creamos una varaible auxiliar.
+
+        if (numero < 10) {
+            System.out.println("Tu número tiene una cifra, así que la posición es 1.");
+        } else {
+            while (numero > 0 && !encontrado) {
+                if ((numero % 10) == digito) {
+                    encontrado = true;
+                } else {
+                numero = numero / 10;
+                posicion++;
+                }          
+            }
+        } 
+
+        //Resolución del problema 
+        System.out.println();
+        if (encontrado) { 
+            System.out.println("El número elegido es " + numeroIntroducido + " y el dígito elegido es el " + digito + ".");
+            System.out.println("La posición del dígito es la " + posicion + "ª.");
+           
+        } else {
+            System.out.println("El dígito " + digito + " no se encuentra en el número " + numeroIntroducido + ".");
+        }
+    }    
+}
