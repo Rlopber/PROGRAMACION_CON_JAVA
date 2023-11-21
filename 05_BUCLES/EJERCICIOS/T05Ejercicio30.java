5c981740f2f80407e413a39db46a27a7adc01c9d
Raquel <rlopber070@g.educaand.es>
2023-11-21 12:13:59 +0100

    Añadido T05Ejercicio30 (Sin terminar)

---
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio30.java
@@ -0,0 +1,31 @@
+/**
+*
+* Realiza una programa que calcule las horas transcurridas entre dos horas de dos días de la semana. 
+* No se tendrán en cuenta los minutos ni los segundos.
+* El día de la semana se puede pedir como un número (del 1 al 7) o como una cadena (de “lunes” a “domingo”). 
+* Se debe comprobar que el usuario introduce los datos correctamente y que el segundo día es posterior al primero.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T05Ejercicio30 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        //Desclarar variables
+
+        //Descripción del programa y entrada de datos
+        System.out.println("Este programa calculará las horas transcurridas entre dos horas de dos días de la semana.");
+        System.out.print("Introduce el primer día de la semana: ");
+        
+
+        //Comprobación de datos
+
+        //Resolución del problema
+
+        s.close();
+    }    
+}
