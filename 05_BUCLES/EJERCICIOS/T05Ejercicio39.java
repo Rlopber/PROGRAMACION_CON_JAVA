f87d4bcfd7aeeb01a8481e0a50e8b9571aefee6d
Raquel <rlopber070@g.educaand.es>
2023-11-23 12:51:25 +0100

    Añadido T0539 y T0541 (Sin hacer)

---
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio39.java
@@ -0,0 +1,17 @@
+/**
+*
+* Escribe un programa que pida un número entero positivo por teclado 
+* y que muestre a continuación los números desde el 1 al número introducido junto con su factorial.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T05Ejercicio39 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        s.close();
+    }    
+}
---
+++ 05_BUCLES/EJERCICIOS/T05Ejercicio41.java
@@ -0,0 +1,20 @@
+/**
+*
+* Escribe un programa que diga cuántos dígitos pares y cuántos dígitos impares hay dentro de un número. 
+* Se recomienda usar long en lugar de int ya que el primero admite números más largos.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T05Ejercicio41 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        s.close();
+
+      
+        
+    }    
+}
