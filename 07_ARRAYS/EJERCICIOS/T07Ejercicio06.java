d69e57a9444e8218db3fa9a2bf973c8b9a940ab2
Rlopber <rlopber070@g.educaand.es>
2024-01-10 20:42:45 +0100

    Añadidos T07Ejercicio06 y T07Ejercicio08

---
+++ 07_ARRAYS/EJERCICIOS/T07Ejercicio06.java
@@ -0,0 +1,45 @@
+/**
+*
+* Escribe un programa que lea 15 números por teclado y que los almacene en un array. 
+* Rota los elementos de ese array, es decir, el elemento de la posición 0 debe pasar a la posición 1, el de la 1 a la 2, etc. 
+* El número que se encuentra en la última posición debe pasar a la posición 0. 
+* Finalmente, muestra el contenido del array.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T07Ejercicio06 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int[] numero = new int[15];
+        System.out.println("Introduzca números enteros y pulse INTRO:");
+
+        //Escritura del array
+        for (int i = 0; i < numero.length; i++) {
+            numero[i] = s.nextInt();
+        }
+        s.close();
+
+        //Salida por pantalla del array
+        for (int i = 0; i < numero.length; i++) {
+            System.out.printf("%3d", numero[i]);
+        }
+        System.out.println("");
+        
+        //Rotamos
+        int aux = numero[14];
+        for (int i = numero.length - 1; i > 0; i--) {
+            numero[i] = numero[i-1];
+        }
+
+        numero[0] = aux;
+
+        //Salida por pantalla del array rotado
+        for (int i = 0; i < numero.length; i++) {
+            System.out.printf("%3d", numero[i]);
+        }
+    }
+}
---
+++ 07_ARRAYS/EJERCICIOS/T07Ejercicio08.java
@@ -0,0 +1,40 @@
+/**
+*
+* Realiza un programa que pida la temperatura media que ha hecho en cada mes de un determinado año 
+* y que muestre a continuación un diagrama de barras horizontales con esos datos. 
+* Las barras del diagrama se pueden dibujar a base de asteriscos o cualquier otro carácter.
+*
+* @author Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T07Ejercicio08 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        //Declarar variables
+        String[] mes = {
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        int[] temperatura = new int[12];
+
+        //Escribir el array de temperatura
+        for (int i = 0; i < temperatura.length; i++) {
+            System.out.print("Introduce la temperatura del mes de " + mes[i] + ": ");
+            temperatura[i] = s.nextInt();
+        }
+        
+        //Mostrar por pantalla en forma de histograma
+        for (int i = 0; i < mes.length; i++) {
+            System.out.printf("%12s |", mes[i]);
+
+            for (int j = 0; j < temperatura[i]; j++) {
+                System.out.print("▄");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
