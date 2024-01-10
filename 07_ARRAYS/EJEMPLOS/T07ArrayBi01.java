ab3f1e013d698af5b40563f5ae71ad6a11526fd6
Rlopber <rlopber070@g.educaand.es>
2024-01-10 21:16:48 +0100

    Añadido T07ArrayBi01

---
+++ 07_ARRAYS/EJEMPLOS/T07ArrayBi01.java
@@ -0,0 +1,30 @@
+/**
+*
+* Descripción del programa
+*
+* @author: Raquel Lopez
+*/
+
+public class T07ArrayBi01 {
+    public static void main(String[] args) {
+        int[][] n = new int[3][2]; //Array de 3 filas x 2 columnas
+                                    // n = {{20,0}, {67,33}, {0,7}}
+
+        n[0][0] = 20;   //1ª fila 1ª columna
+        n[1][0] = 67;   //2ª fila 1ª columna
+        n[1][1] = 33;   //2ª fila 2ª columna
+        n[2][1] = 7;    //3ª fila 2ª columna
+
+       
+
+        System.out.println("Número de filas: " + n.length);
+        System.out.println("Número de columnas: " + n[0].length);
+        for (int filas = 0; filas < n.length; filas++) {
+            for (int columna = 0; columna < n[filas].length; columna++) {
+                System.out.printf("%10d", n[filas][columna]);
+                //Thread.sleep(1000); //retardo de un segundo (no funciona)
+            }
+            System.out.println();
+        }
+    }
+}
