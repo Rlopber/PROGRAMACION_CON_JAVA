6ed58a1fa4920b4661284c412984a059fa1de117
Rlopber <rlopber070@g.educaand.es>
2024-01-11 19:46:52 +0100

    Cambio para funcionamiento del retardo(1s)

--- 07_ARRAYS/EJEMPLOS/T07ArrayBi01.java
+++ 07_ARRAYS/EJEMPLOS/T07ArrayBi01.java
@@ -6,7 +6,7 @@
 */
 
 public class T07ArrayBi01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int[][] n = new int[3][2]; //Array de 3 filas x 2 columnas
                                     // n = {{20,0}, {67,33}, {0,7}}
 
@@ -15,14 +15,12 @@ public class T07ArrayBi01 {
         n[1][1] = 33;   //2ª fila 2ª columna
         n[2][1] = 7;    //3ª fila 2ª columna
 
-       
-
         System.out.println("Número de filas: " + n.length);
         System.out.println("Número de columnas: " + n[0].length);
         for (int filas = 0; filas < n.length; filas++) {
             for (int columna = 0; columna < n[filas].length; columna++) {
                 System.out.printf("%10d", n[filas][columna]);
-                //Thread.sleep(1000); //retardo de un segundo (no funciona)
+                Thread.sleep(1000); //retardo de un segundo (Para que funciona hay que poner throws InterrumptedException o encerrar todo en un try/catch)
             }
             System.out.println();
         }
