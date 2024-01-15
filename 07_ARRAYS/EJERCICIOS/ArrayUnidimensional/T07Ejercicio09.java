083d9fe0c54aa51f8b0637ab84d4fd2136fba15e
Raquel <rlopber070@g.educaand.es>
2024-01-15 14:22:11 +0100

    Añadido T07Ejercicio09

---
+++ 07_ARRAYS/EJERCICIOS/ArrayUnidimensional/T07Ejercicio09.java
@@ -0,0 +1,43 @@
+package ArrayUnidimensional;
+import funcionesmates.*;
+
+/**
+*
+* Realiza un programa que pida 8 números enteros y que luego muestre esos números junto con la palabra “par” o “impar” según proceda.
+*
+* @author: Raquel Lopez
+*/
+
+import java.util.Scanner;
+
+public class T07Ejercicio09 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        //Declarar variables
+        int[] tabla = new int[8];
+        int numero;
+
+        //Pedir los números al usuario
+
+        System.out.println("Por favor, introduzca un número entero y pulse INTRO.");
+
+        for (int i = 0; i < tabla.length; i++) {
+            System.out.print("Número " + (i+1) + ": ");
+            tabla[i] = s.nextInt();
+        }
+        s.close();
+        
+        //Comprobar si son pares o impares y presentar por pantalla
+
+        for (int i = 0; i < tabla.length; i++) {
+            System.out.printf("%8d", tabla[i]);
+            if (Varias.esPar(tabla[i])) {
+                System.out.print(" -- PAR");
+            } else {
+                System.out.print(" -- IMPAR");
+            }
+            System.out.println();
+        }  
+    }
+}
