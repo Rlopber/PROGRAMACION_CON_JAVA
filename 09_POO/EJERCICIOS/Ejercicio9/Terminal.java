5889e5da23bab02e7164538a6f881dbb98df649c
Raquel López Bermúdez <148492169+Rlopber@users.noreply.github.com>
2024-02-02 17:16:22 +0100

    Añadido T09Ejercicio9

---
+++ 09_POO/EJERCICIOS/Ejercicio9/Terminal.java
@@ -0,0 +1,43 @@
+package Ejercicio9;
+
+public class Terminal {
+    ////Atributos
+    private String numero;
+    private int tiempoConversacion;  //en segundos
+
+    ////Constructores
+    public Terminal(String numero) {
+        this.numero = numero;
+    }
+
+    ////Getters
+    public String getNumero() {
+        return numero;
+    }
+
+    public int getTiempoConversacion() {
+        return tiempoConversacion;
+    }
+
+    ////Setters
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setTiempoConversacion(int tiempoConversacion) {
+        this.tiempoConversacion = tiempoConversacion;
+    }
+
+    ////Métodos
+
+    public void llama(Terminal t, int tiempoConversacion) {
+        this.tiempoConversacion += tiempoConversacion;
+        t.tiempoConversacion += tiempoConversacion;
+    }
+
+    ////Override
+    @Override
+    public String toString() {
+        return "Nº " + numero + " - " + tiempoConversacion + "s de conversación";
+    }
+}
