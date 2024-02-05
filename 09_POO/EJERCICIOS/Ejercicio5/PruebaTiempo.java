0336e572570e0ade053ee6afd62b8bce2df48009
Raquel <rlopber070@g.educaand.es>
2024-02-05 14:10:59 +0100

    Terminado T09Ejercicio06

---
+++ 09_POO/EJERCICIOS/Ejercicio5/PruebaTiempo.java
@@ -0,0 +1,13 @@
+package Ejercicio5;
+
+public class PruebaTiempo {
+    public static void main(String[] args) {
+        Tiempo t1 = new Tiempo(1, 20, 30);
+        Tiempo t2 = new Tiempo(0, 30, 40);
+        Tiempo t3 = new Tiempo(0, 35, 20);
+              
+        System.out.println(t1 + " + " + t2 + " = " + Tiempo.sumarTiempos(t1, t2));
+        System.out.println(t2 + " + " + t3 + " = " + Tiempo.sumarTiempos(t2, t3));
+    }
+}
+
--- 09_POO/EJERCICIOS/Ejercicio5/Tiempo.java
+++ 09_POO/EJERCICIOS/Ejercicio5/Tiempo.java
@@ -6,40 +6,50 @@ public class Tiempo {
     private int minutos;
     private int segundos;
 
+    int segundosTotales;
+
     ////Constructores
     public Tiempo (int horas, int minutos, int segundos) {
         this.horas = horas;
         this.minutos = minutos;
         this.segundos = segundos;
+        pasarASegundos();
     }
+    
 
-    ////Getters
-    public int getHoras() {
-        return horas;
-    }
-
-    public int getMinutos() {
-        return minutos;
-    }
-
-    public int getSegundos() {
-        return segundos;
-    }
-   
     ////Métodos
+    /* Se pasa todo a segundos */
+    private int pasarASegundos() {
+        return (this.horas * 3600) + (this.minutos * 60) + this.segundos;
+    }
 
-    public void sumaTiempo() {
-        
+    /*  Pasa los segundos a horas, minutos, segundos */
+    private void pasarAFormatoHMS(int segundosTotales) {
+        this.horas = segundosTotales / 3600;
+        segundosTotales %= 3600;
+        this.minutos = segundosTotales / 60;
+        this.segundos = segundosTotales % 60;
     }
 
     /* Suma */
+    public static Tiempo sumarTiempos(Tiempo t1, Tiempo t2) {
+        int totalSegundos = t1.pasarASegundos() + t2.pasarASegundos();
+        Tiempo resultado = new Tiempo(0, 0, totalSegundos);
+        resultado.pasarAFormatoHMS(totalSegundos);
+        return resultado;
+    }
+
     /* Resta */
+    public static Tiempo restarTiempos(Tiempo t1, Tiempo t2) {
+        int totalSegundos = t1.pasarASegundos() - t2.pasarASegundos();
+        Tiempo resultado = new Tiempo(0, 0, totalSegundos);
+        resultado.pasarAFormatoHMS(totalSegundos);
+        return resultado;
+    }
 
     ////Override
     @Override
     public String toString() {
         return horas + "h " + minutos + "m " + segundos + "s";
     }
-
-    
 }
