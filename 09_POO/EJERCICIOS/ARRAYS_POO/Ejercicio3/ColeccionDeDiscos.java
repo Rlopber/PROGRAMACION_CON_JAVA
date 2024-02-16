cac0c440617980ddf0c3e3ff67b1b83556b78cd3
Raquel López Bermúdez <148492169+Rlopber@users.noreply.github.com>
2024-02-16 16:27:32 +0100

    Añadido T09Ejercicio03

---
+++ 09_POO/EJERCICIOS/ARRAYS_POO/Ejercicio3/ColeccionDeDiscos.java
@@ -0,0 +1,17 @@
+package ARRAYS_POO.Ejercicio3;
+
+public class ColeccionDeDiscos {
+    public static void main(String[] args) {
+        Disco[] discos = new Disco[10];
+
+        String autor;
+        String titulo;
+        String genero;
+        int duracion;
+        
+        //Crear los discos
+        for (int i = 0; i < discos.length; i++) {
+            System.out.println(discos[i]);
+        }
+    }
+}
