655ec2221b873aa700353a2db5ff6b99840ee22a
Raquel López Bermúdez <148492169+Rlopber@users.noreply.github.com>
2024-02-16 17:20:18 +0100

    Modificado T10Ejemplo03

--- 10_COLECCIONES_Y_DICCIONARIOS/EJEMPLOS/EjemploArrayList03.java
+++ 10_COLECCIONES_Y_DICCIONARIOS/EJEMPLOS/EjemploArrayList03.java
@@ -12,15 +12,40 @@ public class EjemploArrayList03 {
 
         ArrayList<String> lista = new ArrayList<String>();
 
-        //Se añaden 3 elementos
+        //Se añaden 5 elementos
         lista.add("rojo");
         lista.add("verde");
         lista.add("azul");
+        lista.add("rojo");
+        lista.add("amarillo");
 
         System.out.println("Contenido de la lista: ");
 
         for (String color: lista) {     //Saca uno a uno todos los elementos de lista y los metes en una variable llamada color
             System.out.println(color);  //Muestra por pantalla el contenido de la variable color
+        }
+
+        if (lista.contains("rojo")) {
+            System.out.println("El rojo está en la lista de colores:");
+        }
+
+        lista.remove("rojo");
+        System.out.println("Ha eliminado el primer elemento de la lista rojo.");
+        if (lista.contains("rojo")) {
+            System.out.println("Hay otro rojo");
+        }
+
+        if (lista.contains("naranja")) {
+            System.out.println("Hay naranja");
+        } else {
+            System.out.println("No hay naranja");
+        }
+
+        lista.remove(3);
+        System.out.println("Contenido de la lista después de quitar el elemento 3: ");
+
+        for (String color: lista) {     
+            System.out.println(color);
         }
     }
 }
